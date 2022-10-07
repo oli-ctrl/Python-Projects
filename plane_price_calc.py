b5c469be7986ec93b17680755a32537e3e78be17
Oli <88791447+oli-ctrl@users.noreply.github.com>
2022-10-07 11:03:45 +0100

    fix and break :D

--- plane_price_calc.py
+++ plane_price_calc.py
@@ -1,6 +1,3 @@
-
-
-
 class Plane ():
     def __init__(self):
         self.type = None
@@ -45,6 +42,8 @@ class Plane ():
         print (f"Capacity if all seats are first-class: {planes[self.type_pos][5]}")
         print (f"user amount of first class: {self.first_amount}")
         print (f"user amount of standard class: {self.standard_amount}")
+        print (f"user price of first class: {self.first_price}")
+        print (f"user price of standard class: {self.standard_price}")
         print (f"user chosen home {self.home}")
         print (f"user chosen destination {self.destination}")
 
@@ -58,109 +57,152 @@ class Plane ():
         self.standard_amount = None
         self.first_price = None
         self.standard_price = None
-
+        return True
 ## enter flight details + stuff in class
-def plane_input ():
-    print("-------------------------")
-    while True:
-        type_input = str(input(f"What type is your plane({planes[0][0]},{planes[1][0]},{planes[2][0]}): "))
-        type_input = type_input.upper()
-        valid = plane.set_type(type_input)
-        if valid == True:
-            break
-def seat_calc (inps):
-    if inps != 0:
-        if inps > planes[plane.type_pos][5]:
-            print (f"more than the allowed {planes[plane.type_pos][5]} first-class seats ")
-            main_menu() 
-        else:
-            if planes[plane.type_pos][5] > planes[plane.type_pos][4]/2:
-                print(f"more than half the capacity of the {planes[plane.type_pos][4]} standard-class seats")
+    def plane_input (self):
+        print("-------------------------")
+        while True:
+            type_input = str(input(f"What type is your plane({planes[0][0]},{planes[1][0]},{planes[2][0]}): "))
+            type_input = type_input.upper()
+            valid = self.set_type(type_input)
+            if valid == True:
+                return True
+            else: 
+                return False
+            
+    def seat_calc (self):
+        inps = get_input(1,"how many first-class seats are on the aircraft?: ")
+        if inps == None:
+            print ("not a valid input")
+            return False
+        if inps != 0:
+            if inps > planes[self.type_pos][5]:
+                print (f"more than the allowed {planes[self.type_pos][5]} first-class seats ")
                 main_menu() 
-        plane.first_amount = inps
-        plane.standard_amount = (planes[plane.type_pos][4] - (inps*2))
+                return False
+            return True
+        else:
+            if planes[self.type_pos][5] > planes[self.type_pos][4]/2:
+                print(f"more than half the capacity of the {planes[self.type_pos][4]} standard-class seats")
+                main_menu() 
+                return False
+        self.first_amount = inps
+        self.standard_amount = (planes[self.type_pos][4] - (inps*2))
         main_menu()
-        return
+        return True
 
 
 ## airport details 
-def dep_location ():
-    location = input("please input your departing airport (JPl, BOH)")
-    location = location.upper()
-    if location == "JPL" or location == "BOH":
-        plane.home = location
-    else: 
-        print ("not a valid input")
-        main_menu()
-        return
-def arive_location():
-    
-    location = input(f"please input your targeted airport ({airports[0][0]},{airports[1][0]},{airports[2][0]},{airports[3][0]},{airports[4][0]},)")
-    location = location.upper()
-    loop = 0
-    for i in airports:
-        if location == airports[loop][0]:
-            plane.destination = airports[loop][0]
-            plane.destination_pos = loop
-            print(airports[plane.destination_pos][1])
+    def dep_location (self):
+        location = input("please input your departing airport (LPl, BOH)")
+        location = location.upper()
+        if location == "LPL":
+            print ("Liverpool John Lennon")
+            self.home = location
             return True
-        loop += 1
-        if loop == len(airports):
-            print ("Invalid airport")
+        if location == "BOH":
+            self.home = location
+            return True
+        else: 
+            print ("not a valid input")
             main_menu()
-            return
+            return False
+    def arive_location(self):
+        location = input(f"please input your targeted airport ({airports[0][0]},{airports[1][0]},{airports[2][0]},{airports[3][0]},{airports[4][0]})")
+        location = location.upper()
+        loop = 0
+        for i in airports:
+            if location == airports[loop][0]:
+                self.destination = airports[loop][0]
+                self.destination_pos = loop
+                print(airports[self.destination_pos][1])
+                return True
+            loop += 1
+            if loop == len(airports):
+                print ("Invalid airport")
+                main_menu()
+                return False
 
 ##price plan
-def check_airports():
-    if plane.home == None or plane.destination == None:
-        print("you need to add your airports")
-        main_menu()
-        return
-def check_planes():
-    if plane.type == None or plane.destination == None:
-        print("you need to add your plane type")
-        main_menu()
-        return
-def check_seats ():
-    if plane.first_amount == None or plane.standard_amount == None:
-        print("you need to add the amount of first class seats on the aircraft")
-        main_menu()
-        return
-def check_flight_range ():
-    if plane.home == "JPL":
-        if airports[plane.destination_pos][2] > planes[plane.type_pos][3]:
-            print("airport too far away, pick an aircraft with a longer range")
-            print(f"your plane has a range of{planes[plane.type_pos][3]} and the distance to the airport is {airports[plane.destination_pos][3]}")
-    elif plane.home == "BOH":
-        if airports[plane.destination_pos][3] > planes[plane.type_pos][3]:
-            print("airport too far away, pick an aircraft with a longer range")
-            print(f"your plane has a range of{planes[plane.type_pos][3]} and the distance to the airport is {airports[plane.destination_pos][3]}")
-def price_options():
-    plane.standard_price = int(input("enter the price of the standard class seats"))
-    plane.first_price = int(input("enter the price of the first class seats"))
+    def check_airports(self):
+        if self.home == None or self.destination == None:
+            print("you need to add your airports")
+            main_menu()
+            return False
+        return True
+    def check_planes(self):
+        if self.type == None or self.destination == None:
+            print("you need to add your plane type")
+            main_menu()
+            return False
+        return True
+    def check_seats (self):
+        if self.first_amount == None or self.standard_amount == None:
+            print("you need to add the amount of first class seats on the aircraft")
+            main_menu()
+            return False
+        return True 
+    def check_flight_range (self):
+        if self.home == "LPL":
+            if airports[self.destination_pos][2] > planes[self.type_pos][3]:
+                print("airport too far away, pick an aircraft with a longer range")
+                print(f"your plane has a range of {planes[self.type_pos][3]} and the distance to the airport is {airports[self.destination_pos][3]}")
+                main_menu()
+                return False 
+            return True
+        elif self.home == "BOH":
+            if airports[self.destination_pos][3] > planes[self.type_pos][3]:
+                print("airport too far away, pick an aircraft with a longer range")
+                print(f"your plane has a range of{planes[self.type_pos][3]} and the distance to the airport is {airports[self.destination_pos][3]}")
+                main_menu()
+                return False  
+            return True 
+    def price_options(self):
+        self.standard_price = get_input(1,"enter the price of the standard class seats")
+        self.first_price = get_input(1,"enter the price of the first class seats")
+        return False
     
+def get_input (type, message):
+    if type == 1: 
+        try:
+            inp = int(input(message))
+            return inp
+        except:
+            return None
+    return None
 
 def main_menu ():
     print("-------------------------")
     for i in inputs:
         print (i)
     print("-------------------------")
-    choice = int(input("please enter the number before your choice: "))
+    choice = get_input(1,"please enter the number before your choice: ")
     if choice == 1:
-        dep_location()
-        arive_location()
+        if plane.dep_location() == False:
+            return
+        if plane.arive_location() == False:
+            return
     elif choice == 2: 
-        plane_input()
-        plane.get_data()
-        seat_calc(int(input("how many first-class seats are on the aircraft?: ")))
+        if plane.plane_input() == False: 
+            return
+        if plane.get_data() == False:
+            return        
+        if plane.seat_calc() == False:
+            return
     elif choice == 3:
-        check_airports()
-        check_planes()
-        check_seats()
-        check_flight_range ()
-        price_options()
+        if plane.check_airports() == False:
+            return
+        if plane.check_planes() == False:
+            return
+        if plane.check_seats() == False:
+            return 
+        if plane.check_flight_range () == False:
+            return
+        if plane.price_options() == False:
+            return
     elif choice == 4:
-        plane.reset_data()
+        if plane.reset_data() == False:
+            return
         print("data cleared")
     elif choice == 5:
         print("exiting program")
@@ -168,6 +210,8 @@ def main_menu ():
     elif choice == 6:
         print("giving all secret data")
         plane.get_data_more()
+    else:
+        print ("invalid input")
     
 
 
