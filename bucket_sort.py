5f47cb5b3911d4214ffa4e47edfc200f49132352
Oli <88791447+oli-ctrl@users.noreply.github.com>
2022-11-04 08:59:48 +0000

    stuff
    - change list to an inefficient version
    - add comments

--- bucket_sort.py
+++ bucket_sort.py
@@ -1,12 +1,15 @@
 def Counting_sort(data):
+    # get the largest and the smallest value, aswell as the range
     big = max(data)
     smol = min(data)
     long = big-smol
+    ## create the dump list with the required length
     dump = [0] * (long + 1)
     ## tally the list for numbers
     for b in data:
         dump[b-smol] += 1
     sorted = []
+    ## re-create the list but in order
     for position in range(len(dump)):
         for i in range (0,dump[position]):
             sorted.append(position +smol)
@@ -14,5 +17,5 @@ def Counting_sort(data):
 
 
 
-stuff = [1,1,1,2,2,3,1,2,3,1,2,3,1,4,1,5,1]
-print (Counting_sort(stuff))
+stuff = [23,1,1,42,2,2,4,5,12312,6,7,6,4,5,6,9]
+print(Counting_sort(stuff))
