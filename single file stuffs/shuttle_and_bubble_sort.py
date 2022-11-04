9ddfef6dc435854452da3b5447f46e5c2c7907b7
Oli <88791447+oli-ctrl@users.noreply.github.com>
2022-11-04 09:53:04 +0000

    yes

---
+++ single file stuffs/shuttle_and_bubble_sort.py
@@ -0,0 +1,42 @@
+def shuttle_sort (data):
+    count = 0
+    finished = False
+    while not finished:
+        count+=1
+        finished = True
+        # normal bubble sort
+        for position in range(0, len(data)-1):
+            if data[position] > data [position+1]:
+                hold = data[position]
+                data[position] = data[position+1]
+                data[position+1] = hold
+                finished = False
+        # bubble sort but backwards :O
+        if not finished:
+            for rev_position in range (len(data)-1,0):
+                if data[rev_position] < data [rev_position-1]:
+                    rev_hold = data[rev_position-1]
+                    data[rev_position-1] = data[rev_position]
+                    data[rev_position] = rev_hold
+                    finished = False
+    return data,count
+
+def bubble_sort (data):
+    finished = False
+    count = 0
+    while not finished:
+        count+=1
+        finished = True
+        # normal bubble sort
+        for position in range(0, len(data)-1):
+            if data[position] > data [position+1]:
+                hold = data[position]
+                data[position] = data[position+1]
+                data[position+1] = hold
+                finished = False
+    return data, count
+
+stuff = [4,123,6,5,124123,2]
+print("shuttle",shuttle_sort(stuff))
+print("bubble",bubble_sort(stuff))
+
