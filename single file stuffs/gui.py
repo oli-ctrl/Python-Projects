4987d706a9c4e2ff90805d60a275a8e2086581b6
Oli <88791447+oli-ctrl@users.noreply.github.com>
2023-02-05 11:21:10 +0000

    move things around and add some comments

--- single file stuffs/gui.py
+++ single file stuffs/gui.py
@@ -1,33 +1,21 @@
 import tkinter as tk
 from math import sqrt
 
+## init the window
 window=tk.Tk()
-
-window.title("My first GUI")
-window.geometry("500x500")  
-
+window.title("Pythagoras calculator")
+window.geometry("450x115")  
 
 
+## what happens when the reset button is pressed
 def callback_button_reset():
     ent_a.delete(0, tk.END)
     ent_b.delete(0, tk.END)
     ent_c.configure(state="normal")
     ent_c.delete(0, tk.END)
     ent_c.configure(state="disabled")
-    print("reset")
 
-def open_popup(text):
-    popup = tk.Tk()
-    popup.geometry("10x100")
-    popup.title("error")
-    label = tk.Label(popup, text=text)
-    label.pack()
-    btn_close = tk.Button(master=popup, text="close", height= 1, command= popup.destroy) 
-    btn_close.pack()
-
-    popup.mainloop()
-
-
+## what happens when the calculate button is pressed
 def callback_button_calculate():
     a = ent_a.get()
     b = ent_b.get()
@@ -46,11 +34,27 @@ def callback_button_calculate():
     ent_c.configure(state="disabled")
     print("calculate")
 
-## make the objects
+## stuff for error popup window
+def open_popup(text):
+    popup = tk.Tk()
+    popup.geometry("10x45")
+    popup.title("error")
+    label = tk.Label(popup, text=text)
+    label.pack()
+    btn_close = tk.Button(master=popup, text="close", height= 1, command= popup.destroy) 
+    btn_close.pack()
+    popup.mainloop()
+
+
+
+## __make the objects__
+
+## make the title
 txt_title = tk.Label(master=window, 
                     text="pythaoras formula calculator",
                     font=("Arial", 20),)
 
+## make the input fields
 txt_a = tk.Label(master=window,text="a = ")
 ent_a = tk.Entry(master=window)
 txt_b = tk.Label(master=window,text="b = ")
@@ -58,13 +62,17 @@ ent_b = tk.Entry(master=window)
 txt_c = tk.Label(master=window,text="c = ")
 ent_c = tk.Entry(master=window, bg="grey", state="disabled")
 
+## make the buttons
 btn_calculate = tk.Button(master=window, text="calculate",bg="green" , height= 3, command=callback_button_calculate)
 btn_reset = tk.Button(master=window, text="reset",bg="orange", height= 3, command= callback_button_reset)
 btn_quit = tk.Button(master=window, text="quit",  bg="tomato", height= 3, command=window.quit)
 
-## place them on the grid
+## __place the objects__
+
+## place the title
 txt_title.grid(row=0, column=0, columnspan=6)
 
+## place the input fields
 txt_a.grid(row=1, column=0, sticky=tk.EW)
 ent_a.grid(row=1, column=1)
 txt_b.grid(row=1, column=2, sticky=tk.EW)
@@ -72,10 +80,10 @@ ent_b.grid(row=1, column=3)
 txt_c.grid(row=1, column=4, sticky=tk.EW)
 ent_c.grid(row=1, column=5)
 
+## place the buttons
 btn_calculate.grid(row=2, column=0, columnspan=2, rowspan=2, sticky=tk.EW)
 btn_reset.grid(row=2, column=2, columnspan=2, rowspan=2, sticky=tk.EW)
 btn_quit.grid(row=2, column=4, columnspan=2, rowspan=2, sticky=tk.EW)
-
 
 
 
