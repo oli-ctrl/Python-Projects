045c1e166e431bfa1df4da0a1eab5cac8965d90c
Oli <88791447+oli-ctrl@users.noreply.github.com>
2023-04-20 16:49:52 +0100

    add more comments, change a few bits of logic
    added fruit count, randomly place first fruit(s) and fix wall hits

--- Python/single file stuffs/snake.py
+++ Python/single file stuffs/snake.py
@@ -4,15 +4,29 @@ from random import randint
 import keyboard
 import tkinter as tk
 
+## need to import keyboard and tkinter to run this
+
+
+
+## __read below to change settings__
+
+## if you want to change the size of the board, change the x and y variables
+## if you want to change the starting length of the snake, change the lenght variable
+## if you want to change the keys, change the upkey, leftkey, downkey and rightkey variables
+## if you want to change the starting direction, change the direction variable
+## if you want to change the amount of fruit, change the fruit_count variable
+
+
 ## Default params for board
 x = 15
 y = 15
 
-## snake start params (maybe change speed depending on pc speed, 0.5 is good for me)
+## snake start params (maybe change speed depending on pc speed, 0.5 is good for me, on a slower machine it was good at 0.2)
 score = 0
 speed = 0.5
 direction = "Right"
 lenght = 2
+fruit_count = 5
 
 ## Movement keys
 upkey = "w"
@@ -41,11 +55,16 @@ class Snake:
         self.length = lenght
         self.poslist = []
         self.fruitpos= [math.ceil(x/2), math.ceil(y/2)+3]
-        board [self.pos[0]][self.pos[1] + 3] = 2
+        ##place fruit, making sure its an empty location. 
+        for i in range (fruit_count):
+            while True:
+                pos=[randint(0,x-1),randint(0,y-1)]
+                if board[pos[0]][pos[1]] == 0:
+                    board[pos[0]][pos[1]]= 2
+                    break
     
-        
+    ## function to move the snake and do nothing if it is dead
     def move(self):
-
         nextpos = self.pos
         ## depending on direction, increase position 
         if self.direction == "Right":
@@ -72,10 +91,7 @@ class Snake:
         else:
             self.direction="Dead"  
 
-
-
-
-
+    ## funcion to remove the tail of the snake, all all trailing squares are stored in a poslist. 
     def cleanup (self):
         ## if lenght of the poslist is longer than the snakes lenght, remove first element and set that to 0 on the board
         if len(self.poslist) > self.length:
@@ -83,21 +99,20 @@ class Snake:
             board[int(clean[0])][int(clean[1])] = 0
 
 
-        
+    ## function to check what the snake will hit on its next move, returns true if it is a valid move, and deals with fruit placement and re-placment
     def checkValid(self):
         global score
-        ## check walls (broken)
-        if int(self.nextpos[0])-1 >= x or int(self.nextpos[1])-1 >= y or int(self.nextpos[0]) >= x or int(self.nextpos[1]) >= y:
-            print("hit  wall")
+        ## check if next move hits the walls (broken)
+        if int(self.nextpos[0]) >= x or int(self.nextpos[1]) >= y or int(self.nextpos[0]) < 0 or int(self.nextpos[1]) < 0:
+            print("hit wall")
             return False
-        
-
-        ## check tail
+    
+        ## check if next move hits the tail
         elif board[int(self.nextpos[0])][int(self.nextpos[1])] == 1:
             print("hit tail")
             return False
 
-        ## check fruit
+        ## check if next move hits any fruit
         if board[int(self.nextpos[0])][int(self.nextpos[1])] == 2:
             self.length +=1 
             score += 1
@@ -176,6 +191,8 @@ def main_loop():
         direction = "Down"
     elif keyboard.is_pressed(rightkey) and snake.direction!="Left":
         direction = "Right"
+
+    ## add some key rollover code soon:tm: should allow for 2 keys to be pressed in succession without missing one of them
     
     
     ## if count is more than 300 (resets to 275, this is to add a delay before the snake moves the first time) set direction, move head and update board. this also checks if the snake is alive. and breaks the loop if it is
