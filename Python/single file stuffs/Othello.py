364531822a3e2184bc82e08a062913f512900b0e
Oli <88791447+oli-ctrl@users.noreply.github.com>
2023-04-24 21:27:20 +0100

    remove ui, try to get game to work before re-adding

--- Python/single file stuffs/Othello.py
+++ Python/single file stuffs/Othello.py
@@ -17,6 +17,16 @@ class Board:
 
     def placePiece (self, x, y, player):
         self.board[x][y] = player
+    
+    def updateUi(self):
+        for i in self.board:
+            print(i)
+
+    def checkplacement(self, player, x,y):
+        print(player, x, y)
+
+        ### some thing to check all cardinal directions and diagonals
+     
 
 
 
@@ -30,45 +40,18 @@ class PlayerHandler:
         else: 
             self.turn = 1 
 
-window = tk.Tk()
-        
-grid = tk.Canvas(master=window)
+
 allbuttons = []
-window.geometry("{}x{}".format((width*100), (height*100)))
 count = 0
-
-## make window
-for i in range(width):
-    for j in range(height):
-        ## make the button and add it to the grid aswell as giving it a command to run when clicked, and a position for future checks.
-        btn = tk.Button(master=grid,state="active" , height= 3, width=6 ,fg="black", command=lambda i=count: button(allbuttons[i]))
-        btn.grid(row=j, column=i, sticky= tk.EW)
-        count += 1
-        ## add the button to a list of all buttons, this is so that it can be checked and change later using the position
-        allbuttons.append(btn)
-        
-        grid.pack()
-        
-window.after(main_loop, 100)
-
-
-def button(self, button):
-    print(button)
-
-def updateUi (self):
-    for i in board.board:
-        print (i)
-
-def main_loop():
-    print ("Player", playerHandler.turn, "'s turn")
-    updateUi()
-    playerHandler.changeTurn()
-    sleep(1)
-    window.after(1, main_loop)
-
 
 board = Board()
 playerHandler = PlayerHandler()
 
-
-window.mainloop()
+while True:
+    print ("Player", playerHandler.turn, "'s turn")
+    board.updateUi()
+    x = int(input("X:"))
+    y = int(input("Y:"))
+    board.checkplacement(playerHandler.turn, x, y)
+    sleep(1)
+    playerHandler.changeTurn()
