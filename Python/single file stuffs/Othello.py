021ee6863bd512633121138d82e445c4c41d74f1
Oli <88791447+oli-ctrl@users.noreply.github.com>
2023-04-26 11:42:05 +0100

    this isnt working.
    final bump with this method of trying it, saved for reference

--- Python/single file stuffs/Othello.py
+++ Python/single file stuffs/Othello.py
@@ -1,33 +1,77 @@
 import tkinter as tk
 from time import sleep
 
-width = 8
-height= 8
 
 
 class Board:
     def __init__ (self):
+        self.width = 8
+        self.height= 8
+        self.flips = []
         ## make an empty board
-        self.board = [[0 for width in range(width)] for height in range(height)]
+        self.board = [[0 for width in range(self.width)] for height in range(self.height)]
         ## place starting pieces
         self.board[3][3] = 1
         self.board[4][4] = 1
         self.board[3][4] = 2
         self.board[4][3] = 2
+        
+        
 
     def placePiece (self, x, y, player):
+        print(x,y)
         self.board[x][y] = player
     
     def updateUi(self):
+        ## print in terminal, in future will update the ui
         for i in self.board:
             print(i)
 
     def checkplacement(self, player, x,y):
-        print(player, x, y)
+        if self.getvalue(x,y) != 0:
+            return False
+        
+        ## check the up direction
+        def checkup(distup):
+            currentPos = [x,y]
+            temp = []
+            for i in range(0, distup):
+                temp.append([x,y])
+                currentPos[1] -= 1
+                print(distup, currentPos)
+                if (self.getvalue(currentPos[0], currentPos[1]) == player):
+                    return temp
+        
 
-        ### some thing to check all cardinal directions and diagonals
-     
+        # print(player, x, y)
+        ## find distance from edges
+        distup = y 
+        distdown = self.height - y
+        distright = self.width - x
+        distleft = x 
 
+        print (f" distup:{distup} \n distdown:{distdown} \n distright:{distright} \n distleft: {distleft}")
+
+        ## get flips 
+        self.flips.append(checkup(distup))
+
+
+
+        if len(self.flips) >1:
+            for i in self.flips:
+                self.placePiece(i[0], i[1], player)
+                self.flips = []
+                return True
+        else:
+            self.flips = []
+            return False
+            
+    
+    def getvalue(self, x,y):
+        try:
+            return self.board[x][y]
+        except:
+            print("fucked")
 
 
 class PlayerHandler:
@@ -50,8 +94,22 @@ playerHandler = PlayerHandler()
 while True:
     print ("Player", playerHandler.turn, "'s turn")
     board.updateUi()
-    x = int(input("X:"))
-    y = int(input("Y:"))
-    board.checkplacement(playerHandler.turn, x, y)
+    while True:
+        try:
+            x = int(input("X:"))
+            y = int(input("Y:"))
+            if y < 8 or x < 8:
+                break
+            else:
+                print("values need to be from 0-7")
+                
+        except:
+            print("one of the inputs was not a valid number")
+        if board.checkplacement(playerHandler.turn, x, y):
+            break
+        print("was not a valid input")
+        
+    
+
     sleep(1)
     playerHandler.changeTurn()
