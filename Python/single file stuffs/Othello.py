d46e2e63ae1d2470d04efeae6a8c224dd859d7cb
Oli <88791447+oli-ctrl@users.noreply.github.com>
2023-05-11 13:03:44 +0100

    count players pieces

--- Python/single file stuffs/Othello.py
+++ Python/single file stuffs/Othello.py
@@ -17,13 +17,22 @@ class Board:
         self.board[4][3] = 2
         self.player = playercontrol
        
-        
+    def countPiece(self, piece):
+        count = 0
+        for i in self.board:
+            for j in i:
+                if j == piece:
+                    count += 1
+        return count
 
     def placePiece (self, x, y):
         print (f"placing piece at {x},{y}")
-        if self.board[x][y] != 0  or x < 0 or x >= self.width or y < 0 or y >= self.height:
+        if self.board[x][y] != 0:
             print("place not empty")
             return
+        if x < 0 or x >= self.width or y < 0 or y >= self.height:
+            print("out of bounds")
+            return 
         ## check all directions 
         self.flips = []
         self.flips.append(self.checkDir([x,y], [0,1]))
@@ -35,7 +44,6 @@ class Board:
         self.flips.append(self.checkDir([x,y], [1,-1]))
         self.flips.append(self.checkDir([x,y], [-1,1]))
         ## if no flips are found, return
-        print(self.flips)
         for i in self.flips:
             if i != []:
                 print(f"placed piece{self.player.turn} at {x},{y}")
@@ -44,7 +52,7 @@ class Board:
                     self.board[j[0]][j[1]] = self.player.turn
                 self.player.changeTurn()
                 return 
-        print("no flips found")
+        print("noT a valid move")
         return
     
     def updateUi(self):
@@ -60,14 +68,14 @@ class Board:
             pos = [pos[0] + dir[0], pos[1] + dir[1]]
             ## check if the position is out of bounds
             if pos[0] < 0 or pos[0] >= self.width or pos[1] < 0 or pos[1] >= self.height:
-                print("hit edge")
+                ## print("hit edge")
                 return []
             ## check if the position is empty 
             if self.board[pos[0]][pos[1]] == 0:
-                print("hit empty")
+                ## print("hit empty")
                 return []
             if self.board[pos[0]][pos[1]] == self.player.turn:
-                print("found")
+                ## print("found")
                 return flips
             flips.append(pos)
 
@@ -93,10 +101,13 @@ while True:
     board.updateUi()
     print(f"player {playerHandler.turn}'s turn")
     try:
-        x = int(input("x: "))
-        y = int(input("y: "))
-        board.placePiece(x,y)
+        x = int(input("x: ")) -1  
+        y = int(input("y: ")) -1 
+        board.placePiece(y,x)
     except:
         print("invalid input")
         continue
+    
+    print("player 1 peices: " + str(board.countPiece(1)))
+    print("player 2 peices: " + str(board.countPiece(2)))
     sleep(1)
