5e628bf628a9234d163b37d592be7d0649c42b58
Oli <88791447+oli-ctrl@users.noreply.github.com>
2023-07-04 19:39:37 +0100

    some qol stuff
    > more configurable settings at the top
    > Options to rotate the image, before and after
    > use an image I actually took rather than some random one off the internet

--- Python/Tests/Images/Imagerandomshift.py
+++ Python/Tests/Images/Imagerandomshift.py
@@ -1,40 +1,56 @@
-from PIL import Image
+try:
+    from PIL import Image
+except ImportError:
+    raise ImportError(f"Pillow is not installed. Run 'python3 -m pip install Pillow' to install it")
 from random import randint
 from time import time
+import os
 
 #settings for the random shift of the image
-shiftfreq = 1200
-maxshiftamount = 75
-minshiftamount = 25
+shiftfreq = 300
+maxshiftamount = 250
+minshiftamount = 0
+
+#rotation settings for the image    
+rotatebeforeamount = 0
+rotateafteramount = 3
+
+# Name of the image to be processed and the name of the image to be saved
+imagename = "Image.jpg"
+returnedimagename = "Returnedimage.jpg"
 
 
-
+## anything below this, dont touch xD
+## funny rotate function
+def rotate(im, times):
+    print (f"Rotating image {times} times")
+    for i in range(times):
+        im = im.transpose(Image.ROTATE_90)
+    return im
+## get the path of the file
+path = os.path.dirname(os.path.abspath(__file__))
+path = path.replace("\\", "/") + "/"
 # Open the image
-im = Image.open("Python\Tests\Images\Image.jpg")
+im = Image.open(f"{path}{imagename}")
+# Rotate the image
+im = rotate(im, rotatebeforeamount)
+# Create a new image to return
 returnedimage = Image.new("RGB", (im.size[0], im.size[1]), "white")
-
 # Get the size of the image
 width, height = im.size
 print (f"Processing image of Width: {width}, Height: {height}")
-
 randomshift = 0
-time1 = time()
-
-
-
 # Process every row of the image
 for x in range(0, height):
     if x % int(height/shiftfreq) == 0:
         randomshift = randint(minshiftamount, maxshiftamount)
         randomshiftdir = randint(0, 1)
-
     # Create a list of the pixels in the row
     row = []
     for y in range(0, width):
         # Get the pixel
         pixel = im.getpixel((y, x))
         row.append(pixel)
-
     # Shift the row   
     for i in range(0,randomshift):
         if randomshiftdir == 0:
@@ -44,17 +60,12 @@ for x in range(0, height):
     # Put the row back into the image
     for y in range(0, width):
         returnedimage.putpixel((y, x), row[y])
-    # print(f"Row {x+1}/{height} processed with a shift of {randomshift}/{randomshiftdir}")
-
-# Print the time taken
-time2 = time()
-print (f"Time taken: {(time2-time1)/60} Minutes")
-
+# Rotate the image
+returnedimage = rotate(returnedimage, rotateafteramount)
 # Save the image
 print ("Saving image")
-returnedimage.save("Python\Tests\Images\Initialtest.png")
+returnedimage.save(f"{path}{returnedimagename}")
 print ("Image saved")
-
 ## show the image
-image = Image.open("Python\Tests\Images\Initialtest.png")
+image = Image.open(f"{path}{returnedimagename}")
 image.show()
