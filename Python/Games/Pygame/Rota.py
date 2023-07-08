72ec64d62043cf70d18c9b66ccd76911a4844680
Oli <88791447+oli-ctrl@users.noreply.github.com>
2023-07-08 12:13:06 +0100

    Rota works :D
    Absolutely gaming

--- Python/Games/Pygame/Rota.py
+++ Python/Games/Pygame/Rota.py
@@ -1,42 +1,60 @@
-# Example file showing a basic pygame "game loop"
 import pygame
+import time
 
 # pygame setup
 pygame.init()
+pygame.font.init()
+my_font = pygame.font.SysFont('Arial', 30)
 screen = pygame.display.set_mode((1280, 720))
 clock = pygame.time.Clock()
 running = True
 
-def getclick(piece):
-    if board.turn == piece.color or board.turn == None:
-        if pygame.mouse.get_pressed()[0] == 1:
-            if piece.x - 20 <= pygame.mouse.get_pos()[0] <= piece.x + 20 and piece.y - 20 <= pygame.mouse.get_pos()[1] <= piece.y + 20 or piece==board.selected:
-                return True
-    return False
+## game setup
+pressed = False
+def getclick():
+    global pressed
+    for i in board.places:
+        if pygame.mouse.get_pressed()[0] and pygame.Rect(i[0][0]-40, i[0][1]-40, 80, 80).collidepoint(pygame.mouse.get_pos()) and not pressed:
+            pressed = True
+            print(f"clicked spot{i[1]}")
+            return i[1]
+        if pygame.mouse.get_pressed()[0] == False:
+            pressed = False
+    return None
+
+## peice class
 class piece():
-    def __init__(self, x, y, color):
-        self.x = x
-        self.y = y
+    def __init__(self, color):
         self.color = color
         self.position = None
         self.firstmove = True
-        self.movedict = {0: [0,1,2,3,4,5,6,7,8], 1: {0,9,2}, 2:{0,1,3}, 3:{0,2,4}, 4:{0,3,5}, 5:{0,4,6}, 6:{0,5,7}, 7:{0,6,8}, 8:{0,7,1}}    
+        self.size = 20
+        self.movedict = {0: [0,1,2,3,4,5,6,7,8], 1: {0,8,2}, 2:{0,1,3}, 3:{0,2,4}, 4:{0,3,5}, 5:{0,4,6}, 6:{0,5,7}, 7:{0,6,8}, 8:{0,7,1}}    
     def draw(self):
-        pygame.draw.circle(screen, self.color, (self.x, self.y), 20)
-        pygame.draw.circle(screen, "black", (self.x, self.y), 20, 2)
+        if self.position != None:
+            for i in board.places:
+                if i[1] == self.position:
+                    pygame.draw.circle(screen, self.color, (i[0]), self.size)
+                    pygame.draw.circle(screen, "black", (i[0]), self.size, 2)
+    def select(self):
+        for i in range(0,10):
+            self.size = self.size + i/3
+            self.draw()
+            pygame.display.flip()
+    def deselect(self):
+        self.size = 20
+        self.draw()
 
 class Board():
     def __init__(self):
+        self.allpieces = []
+        self.wins = {"Black": 0, "White": 0}
         self.selected = None
-        self.pieces = []
-        self.turn = None
-        for i in range (0,6):
-            if i%2 == 0:
-                self.addpiece(piece(100, 100 + (i * 100), "Black"))
-            else:
-                self.addpiece(piece(200, 100 + (i * 100), "white"))
+        self.turn = "Black"
+        self.opposites = {1: 5, 2: 6, 3: 7, 4: 8, 5: 1, 6: 2, 7: 3, 8: 4}
                        ## center         ## top           ## bottom       ## left          ## right       ## top left    ## top right    ## bottom left  ## bottom right
-        self.places = [[(640, 360),(0)], [(640, 60),(1)], [(640, 660),5], [(340, 360),7], [(940, 360),3], [(440,160),9], [(840, 160),2], [(440, 560),6], [(840, 560),4]]
+        self.places = [[(640, 360),(0)], [(640, 60),(1)], [(840, 160),2], [(940, 360),3], [(840, 560),4], [(640, 660),5], [(440, 560),6], [(340, 360),7], [(440,160),8]]
+
     def draw(self):
         ## draw board circles
         pygame.draw.rect(screen, "gray", (0,0,1280,720))
@@ -54,48 +72,119 @@ class Board():
         for i in self.places:
             pygame.draw.circle(screen, "gray", (i[0]),40)
             pygame.draw.circle(screen, "white", (i[0]),40, 10)
+
+        ## draw text
+        text = my_font.render(f"Black: {self.wins['Black']}", False, (0, 0, 0))
+        screen.blit(text, (10, 10))
+        text = my_font.render(f"White: {self.wins['White']}", False, (0, 0, 0))
+        screen.blit(text, (10, 40))
+        text = my_font.render(f"{self.turn}'s turn", False, (0, 0, 0))
+
         ## draw pieces
-        for i in reversed(self.pieces):
+        for i in reversed(self.allpieces):
             i.draw()
-    
-    def addpiece(self, piece):
-        self.pieces.append(piece)
-    
-    def checksnap(self, piece):
-        try:
-            if piece.firstmove == True:
-                for i in self.places:
-                    if i[0][0] - 40 <= piece.x <= i[0][0] + 40 and i[0][1] - 40 <= piece.y <= i[0][1] + 40:
-                        piece.x = i[0][0]
-                        piece.y = i[0][1]
-                        piece.position = i
-                        piece.firstmove = False
-                        if self.turn == None:
-                            self.turn = piece.color
-                        self.switchturn()
-                        return True
-            else:
-                for i in self.places:
-                    if i[0][0] - 40 <= piece.x <= i[0][0] + 40 and i[0][1] - 40 <= piece.y <= i[0][1] + 40:
-                        if piece.position[1] in piece.movedict[i[1]]:
-                            piece.x = i[0][0]
-                            piece.y = i[0][1]
-                            piece.position = i
-                            piece.firstmove = False
-                            self.switchturn()
-                            return True
-                        else:
-                            piece.x = piece.position[0][0]
-                            piece.y = piece.position[0][1]
-                            return False
-            return False
-        except:
-            return False
-    def switchturn(self):
+    ## swaps turn
+    def changeturn(self):
+        board.draw()
+        self.checkwin()
         if self.turn == "Black":
-            self.turn = "white"
+            self.turn = "White"
         else:
             self.turn = "Black"
+
+    ## logic for checking if the board is in a win state
+    def checkwin(self):
+        self.boardstate=[None,None,None,None,None,None,None,None,None]
+        prev = None
+        for i in self.allpieces: 
+            self.boardstate.pop(i.position)
+            self.boardstate.insert(i.position, i.color)
+        if self.boardstate[0] != None:
+            print("checking for 3 in a row around center")
+            count = 0
+            for type in self.boardstate:
+                if count == 0:
+                    pass
+                elif type == None:
+                    pass
+                elif type == self.boardstate[self.opposites[count]]:
+                    if self.boardstate[0] == type:
+                        print(f"{board.turn} wins") 
+                        board.reset(board.turn)
+                        return
+                else:
+                    pass
+                count += 1
+
+
+        print("checking for 3 in a row around edge")
+        ## logic to remove the seem from the list
+        self.boardstate.pop(0)
+        self.boardstate = self.boardstate + self.boardstate 
+        count = 0
+        for i in self.boardstate:
+            if i == None:
+                prev = None
+                count = 0
+            elif prev == i:
+                count = count + 1
+            else:
+                count = 0
+            prev = i
+            if count == 2:
+                print(f"{board.turn} wins")
+                board.reset(board.turn)
+                return
+            
+    def reset(self,color):
+        board.draw()
+        pygame.display.flip()
+        time.sleep(1)
+        board.allpieces = []
+        board.selected = None
+        board.turn = color
+        board.wins[color] += 1
+                
+def gamelogic():
+    click = getclick()
+    if click == None:
+        pass
+    else:
+        if len(board.allpieces) < 6:
+            if click not in [i.position for i in board.allpieces]:
+                print ("empty space")
+                print("placing peices")
+                if board.turn == "Black":
+                    board.allpieces.append(piece("Black"))
+                else:
+                    board.allpieces.append(piece("White"))
+                board.allpieces[-1].position = click
+                board.changeturn()
+        else:
+            try:
+                board.selected.deselect()
+            except:
+                pass
+            print("all peices placed")
+            if board.selected == None:
+                for i in board.allpieces:
+                    if i.position == click:
+                        if i.color == board.turn:
+                            print("selected")
+                            board.selected = i
+                            board.selected.select()
+            else:
+                print("moving")
+                if board.selected.color == board.turn:
+                    if click in board.selected.movedict[board.selected.position]:
+                        print ("valid move")
+                        if click not in [i.position for i in board.allpieces]:
+                            print ("empty space")
+                            board.selected.position = click
+                            board.changeturn()
+                board.selected = None
+
+
 
 board=Board()
 board.selected = None
@@ -111,23 +200,7 @@ while running:
     # RENDER YOUR GAME HERE
     # draw a circle at the mouse position
     board.draw()
-    gotclick = False
-    for i in board.pieces:
-        if getclick(i):
-            board.selected = i
-            board.pieces.remove(i)
-            board.pieces.insert(0,i)
-            if not gotclick:
-                gotclick = True
-            break
-    if not gotclick:
-        board.checksnap(board.selected)
-        board.selected = None
-    if board.selected != None:
-        board.selected.x = pygame.mouse.get_pos()[0]
-        board.selected.y = pygame.mouse.get_pos()[1]
-        
-            
+    gamelogic()
 
 
     # flip() the display to put your work on screen
