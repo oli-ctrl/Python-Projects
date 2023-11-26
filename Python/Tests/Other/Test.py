e3cc797c1d923cc8d0f7420c08dde654003ac23a
Oli <88791447+oli-ctrl@users.noreply.github.com>
2023-11-26 14:06:31 +0000

    Update Test.py

--- Python/Tests/Other/Test.py
+++ Python/Tests/Other/Test.py
@@ -12,6 +12,20 @@ def income(plane, f, e):
 
     return firstPrice + econPrice
 
+def income(plane, f, e):
+    econPrice = 0
+    firstPrice = 0
+    if plane == "Small Airframe":
+        firstPrice += 16*f
+        econPrice += 210*e
+    elif plane == "Medium Airframe":
+        firstPrice += 32*f
+        econPrice += 300*e
+    elif plane == "Large Airframe":
+        firstPrice += 64*f
+        econPrice += 850*f
+    return firstPrice + econPrice
+
 
 ## I have given you some, But dont Want to give you all the answers, so edit the code above to make it work
 
@@ -21,9 +35,9 @@ print (f"large: {income('Large Airframe', 80, 30)}")
 
 ## running this code should return the following:
 ##
-##      small: 1860
+##      small: 12100
 ##      medium: 14880
 ##      large: 73120
 ##
-## If these values are not returned, something is wrong, if u need a hint, ask me. 
+## If these values are not returned, something is wrong, if u need a hint, ask me
 
